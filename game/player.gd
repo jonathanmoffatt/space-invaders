ce4069de73b5c3696131a1b791f333d839bc7bd4
Jonathan <jonathantheman@gmail.com>
2017-09-17 18:48:46 +1000

    Simple fire rate restriction.

--- game/player.gd
+++ game/player.gd
@@ -4,6 +4,7 @@ export var velocity = 700
 export (PackedScene) var bullet
 
 onready var bullet_container = get_node("bullet_container")
+onready var bullet_timer = get_node("bullet_timer")
 
 var screen_size
 var pos = Vector2()
@@ -18,7 +19,8 @@ func _ready():
 	
 func _process(delta):
 	if Input.is_action_pressed("player_fire"):
-		fire()
+		if get_node("bullet_timer").get_time_left() == 0:
+			fire()
 	if Input.is_action_pressed("player_left"):
 		pos.x -= velocity * delta
 	if Input.is_action_pressed("player_right"):
@@ -31,6 +33,7 @@ func _process(delta):
 	set_pos(pos)
 
 func fire():
+	bullet_timer.start()
 	var b = bullet.instance()
 	bullet_container.add_child(b)
 	b.start_at(get_pos())
