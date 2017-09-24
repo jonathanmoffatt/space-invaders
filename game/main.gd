92a81310cb8c118de5b4a23b046e0463f18ab756
Jonathan <jonathantheman@gmail.com>
2017-09-24 11:10:53 +1000

    Fixup bug when shooting invaders (needed to hold weak references to the invaders within our squadron array).

--- game/main.gd
+++ game/main.gd
@@ -8,12 +8,14 @@ func _ready():
 		for i in range(8):
 			var invader = invader_template.duplicate()
 			self.add_child(invader)
-			squadron.append(invader)
+			squadron.append(weakref(invader))
 			invader.set_pos(Vector2(i*150 + 300, j*120 + 100))
 			invader.connect("invader_direction_change", self, "on_direction_change")
-			invader.start()
+	for invader in squadron:
+		invader.get_ref().start()
 		
 func on_direction_change(new_travel):
-	for invader in squadron:
+	for inv in squadron:
+		var invader = inv.get_ref()
 		if invader:
 			invader.change_direction(new_travel)
