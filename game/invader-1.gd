86311ce44e64a6dd4220d6c47c424d3390a8066b
Jonathan <jonathantheman@gmail.com>
2017-09-24 12:45:11 +1000

    First attempt at "wave" style movement.

--- game/invader-1.gd
+++ game/invader-1.gd
@@ -3,14 +3,15 @@ extends Area2D
 export var velocity = 150
 export var margin_side = 280
 export var step_down = 50
+export var side_step = 40
 
 signal invader_direction_change
 
 enum Travelling {
 	LEFT,
 	RIGHT,
-	DOWN_THEN_RIGHT,
-	DOWN_THEN_LEFT
+	DOWN,
+	STATIONARY
 }
 
 var screen_size
@@ -18,7 +19,7 @@ var pos = Vector2()
 var currently_travelling = Travelling.RIGHT
 var direction = Vector2(1, 0)
 var velocity_adjustment = 1.0
-var pos_at_turn = Vector2()
+var pos_at_blip = Vector2()
 
 func _ready():
 	add_to_group("invaders")
@@ -26,22 +27,42 @@ func _ready():
 	
 func start():
 	pos = get_pos()
-	pos_at_turn = get_pos()
-	set_process(true)
 	
 func _process(delta):
 	pos += velocity * velocity_adjustment * delta * direction
-	var right_limit = screen_size.width - margin_side
-	var finished_going_down = pos.y > pos_at_turn.y + step_down
 	set_pos(pos)
-	if pos.x > right_limit && currently_travelling == Travelling.RIGHT:
-		emit_signal("invader_direction_change", Travelling.DOWN_THEN_LEFT)			
-	if pos.x < margin_side && currently_travelling == Travelling.LEFT:
-		emit_signal("invader_direction_change", Travelling.DOWN_THEN_RIGHT)
-	if finished_going_down && currently_travelling == Travelling.DOWN_THEN_RIGHT:
-		emit_signal("invader_direction_change", Travelling.RIGHT)
-	if finished_going_down && currently_travelling == Travelling.DOWN_THEN_LEFT:
-		emit_signal("invader_direction_change", Travelling.LEFT)
+	if currently_travelling == Travelling.RIGHT || currently_travelling == Travelling.LEFT:
+		var distance = (pos - pos_at_blip).abs().x
+		if distance >= side_step:
+			set_process(false)
+			currently_travelling = Travelling.STATIONARY
+	
+
+	# var right_limit = screen_size.width - margin_side
+	# var finished_going_down = pos.y > pos_at_blip.y + step_down
+	# set_pos(pos)
+	# if pos.x > right_limit && currently_travelling == Travelling.RIGHT:
+	# 	emit_signal("invader_direction_change", Travelling.DOWN_THEN_LEFT)			
+	# if pos.x < margin_side && currently_travelling == Travelling.LEFT:
+	# 	emit_signal("invader_direction_change", Travelling.DOWN_THEN_RIGHT)
+	# if finished_going_down && currently_travelling == Travelling.DOWN_THEN_RIGHT:
+	# 	emit_signal("invader_direction_change", Travelling.RIGHT)
+	# if finished_going_down && currently_travelling == Travelling.DOWN_THEN_LEFT:
+	# 	emit_signal("invader_direction_change", Travelling.LEFT)
+
+func blip_left():
+	print("blip_left")
+	currently_travelling = Travelling.LEFT
+	direction = Vector2(-1, 0)
+	pos_at_blip = get_pos()
+	set_process(true)
+
+func blip_right():
+	print("blip_right")
+	currently_travelling = Travelling.RIGHT
+	direction = Vector2(1, 0)
+	pos_at_blip = get_pos()
+	set_process(true)
 
 func change_direction(new_travel):
 	if new_travel == Travelling.RIGHT:
@@ -54,7 +75,7 @@ func change_direction(new_travel):
 		direction = Vector2(0, 1)
 		velocity_adjustment = 0.5
 	currently_travelling = new_travel
-	pos_at_turn = get_pos()
+	pos_at_blip = get_pos()
 		
 func explode():
 	queue_free()
--- game/main.gd
+++ game/main.gd
@@ -2,22 +2,35 @@ extends Node
 
 var squadron = []
 var horizontal_count = 11
-var vertical_count = 5
+var row_count = 5
+var travelling = Travelling.RIGHT
+var current_row = 0
+
+onready var row_movement_timer = get_node("row_movement_timer")
+onready var wave_movement_timer = get_node("wave_movement_timer")
+
+enum Travelling {
+	LEFT,
+	RIGHT,
+	DOWN_THEN_RIGHT,
+	DOWN_THEN_LEFT
+}
 
 func _ready():
 	var invader_template = get_node("invader-template")
-	for j in range(vertical_count):
+	for j in range(row_count):
 		var row = []
 		for i in range(horizontal_count):
 			var invader = invader_template.duplicate()
 			self.add_child(invader)
 			row.append(weakref(invader))
 			invader.set_pos(Vector2(i*150 + 150, j*120 + 100))
-			invader.connect("invader_direction_change", self, "on_direction_change")
+			# invader.connect("invader_direction_change", self, "on_direction_change")
 		squadron.append(row)
 	start()
 	
 func start():	
+	wave_movement_timer.start()
 	for invader in get_all_invaders():
 		invader.start()
 
@@ -29,7 +42,33 @@ func get_all_invaders():
 			if invader:
 				all.append(invader)
 	return all
-			
+	
+func get_invaders_by_row(row):
+	var invaders = []
+	for inv in squadron[row]:
+		var invader = inv.get_ref()
+		if invader:
+			invaders.append(invader)
+	return invaders
+
 func on_direction_change(new_travel):
 	for invader in get_all_invaders():
 		invader.change_direction(new_travel)
+
+func _on_row_movement_timer_timeout():
+	print("_on_row_movement_timer_timeout")
+	for invader in get_invaders_by_row(current_row):
+		if travelling == Travelling.LEFT:
+			invader.blip_left()
+		if travelling == Travelling.RIGHT:
+			invader.blip_right()
+	current_row += 1
+	if current_row < row_count:
+		row_movement_timer.start()
+	else:
+		wave_movement_timer.start()
+
+func _on_wave_movement_timer_timeout():
+	print("_on_wave_movement_timer_timeout")
+	current_row = 0
+	row_movement_timer.start()
